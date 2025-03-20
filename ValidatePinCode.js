8fc59f173c4c3a86b753744437c2b0c0f753a7e0
Diksha <diksha.trivedi2122@gmail.com>
2025-03-20 15:36:51 +0530

    Validating PinCode

---
+++ ValidatePinCode.js
@@ -0,0 +1,8 @@
+const pinCode = "400088";
+const pinCodePattern = /^[1-9][0-9]{5}$/;
+
+if (pinCodePattern.test(pinCode)) {
+    console.log("Valid PIN Code");
+} else {
+    console.log("Invalid PIN Code");
+}
