61fa0b72cb5c876a6ab2fbd8c26da02ab1886f58
Diksha <diksha.trivedi2122@gmail.com>
2025-03-20 16:09:13 +0530

    Validating email part 5

--- ValidateEmail.js
+++ ValidateEmail.js
@@ -1,5 +1,5 @@
 const email = "abc.xyz@bridgelabz.co.in";
-const emailPattern = /^abc([._+-][a-zA-Z0-9]+)?@bridgelabz\.co(\.in)?$/;
+const emailPattern = /^abc([._+-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-z]{2})?$/;
 
 if (emailPattern.test(email)) {
     console.log("Valid Email");
